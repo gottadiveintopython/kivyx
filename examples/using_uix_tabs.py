aa288309568d6291395a44c7c29e0c71c9b9122c
水戸う納豆齋(Nattōsai Mitō) <flow4re2c@gmail.com>
2020-06-06 22:10:16 +0900

    add KXTabs (#3)

---
+++ examples/using_uix_tabs.py
@@ -0,0 +1,55 @@
+from kivy.app import runTouchApp
+from kivy.factory import Factory
+from kivy.lang import Builder
+from kivyx.uix.boxlayout import KXBoxLayout
+from kivyx.uix.tabs import KXTabs
+
+KV_CODE = '''
+#:set LINE_WIDTH 2
+
+<Spacer@Widget>:
+    size_hint_x: None
+    width: 20
+    canvas:
+        Color:
+            rgba: .2, .2, .2, 1
+        Rectangle:
+            pos: self.pos
+            size: self.size
+
+<LabelTab@ToggleButtonBehavior+Label>:
+    font_size: 30
+    size_hint_min_x: self.texture_size[0]
+<ImageTab@ToggleButtonBehavior+Image>:
+    size_hint_min_x: self.texture.size[0] if self.texture else 1
+
+KXBoxLayout:
+    orientation: 'tb'
+    ScrollView:
+        size_hint_y: None
+        height: 80
+        KXTabs:
+            id: tabs
+            orientation: 'lr'
+            style: 'top'
+            line_width: 2
+            line_color: '#AAAAFF'
+            padding: 20, 0
+            spacing: 20
+            size_hint: None, 1
+            width: max(self.minimum_width, self.parent.width)
+    Widget:
+'''
+
+root = Builder.load_string(KV_CODE)
+add_widget = root.ids.tabs.add_widget
+Spacer = Factory.Spacer
+LabelTab = Factory.LabelTab
+ImageTab = Factory.ImageTab
+for __ in range(5):
+    add_widget(LabelTab(text='first'))
+    add_widget(LabelTab(text='second'))
+    add_widget(ImageTab(source=r'data/logo/kivy-icon-48.png'))
+    add_widget(LabelTab(text='third'))
+    add_widget(Spacer())
+runTouchApp(root)
---
+++ examples/using_uix_tabs2.py
@@ -0,0 +1,88 @@
+# from kivy.config import Config
+# Config.set('graphics', 'width', 1280)
+# Config.set('graphics', 'height', 720)
+# Config.set('graphics', 'fullscreen', 1)
+# Config.set('modules', 'touchring', '')
+from kivy.app import runTouchApp
+from kivy.lang import Builder
+from kivyx.uix.tabs import KXTabs
+
+KV_CODE = '''
+#:set TAB_SIZE 80
+#:set LINE_WIDTH 2
+
+<ImageTab@ToggleButtonBehavior+Image>:
+    size_hint_min_x: self.texture.size[0] if self.texture else 1
+    source: r'data/logo/kivy-icon-48.png'
+
+GridLayout:
+    cols: 3
+    rows: 3
+    padding: LINE_WIDTH
+    Widget:
+        size_hint: None, None
+        size: TAB_SIZE, TAB_SIZE
+    KXTabs:
+        orientation: 'lr'
+        style: 'top'
+        spacing: 20
+        padding: 20
+        line_width: LINE_WIDTH
+        line_color: '#AAAAFF'
+        line_stays_inside: False
+        size_hint_y: None
+        height: TAB_SIZE
+        ImageTab:
+        ImageTab:
+        ImageTab:
+        ImageTab:
+    Widget:
+        size_hint: None, None
+        size: TAB_SIZE, TAB_SIZE
+    KXTabs:
+        orientation: 'tb'
+        style: 'left'
+        spacing: 20
+        line_width: LINE_WIDTH
+        line_color: '#AAAAFF'
+        line_stays_inside: False
+        size_hint_x: None
+        width: TAB_SIZE
+        ImageTab:
+        ImageTab:
+        ImageTab:
+    Widget:
+    KXTabs:
+        orientation: 'bt'
+        style: 'right'
+        padding: 20
+        line_width: LINE_WIDTH
+        line_color: '#AAAAFF'
+        line_stays_inside: False
+        size_hint_x: None
+        width: TAB_SIZE
+        ImageTab:
+        ImageTab:
+    Widget:
+        size_hint: None, None
+        size: TAB_SIZE, TAB_SIZE
+    KXTabs:
+        orientation: 'lr'
+        style: 'bottom'
+        line_width: LINE_WIDTH
+        line_color: '#AAAAFF'
+        line_stays_inside: False
+        size_hint_y: None
+        height: TAB_SIZE
+        ImageTab:
+        ImageTab:
+        ImageTab:
+        ImageTab:
+        ImageTab:
+    Widget:
+        size_hint: None, None
+        size: TAB_SIZE, TAB_SIZE
+'''
+
+root = Builder.load_string(KV_CODE)
+runTouchApp(root)
