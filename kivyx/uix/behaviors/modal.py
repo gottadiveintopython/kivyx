b14aa22bef173069128de21d36549042a1c6a601
gottadiveintopython <flow4re2c@gmail.com>
2020-07-10 16:48:53 +0900

    put another widget just for the background

--- kivyx/uix/behaviors/modal.py
+++ kivyx/uix/behaviors/modal.py
@@ -1,7 +1,6 @@
 '''The differences from `kivy.uix.modalview.ModalView`.
 
-- This one is a behavior-type class, so you can use any widgets as a base
-  except Scatter and ScrollView. (RelativeLayout is fine.)
+- This one is a behavior-type class, so you can use any widgets as a base.
 - Doesn't have 'background_image' or 'background_color', so if you want those,
   implement it by yourself.
 - Can be opened multiple times. (ModalView can be opened only once).
@@ -23,9 +22,6 @@
   or not.
 - dismiss() doesn't have 'force' and 'animation' parameters.
 - open() doesn't have 'animation' parameter.
-
-#:TODO Support Scatter and ScrollView by putting another widget only
-#      for the background.
 '''
 
 __all__ = ('KXModalBehavior', )
@@ -39,15 +35,26 @@ import asynckivy as ak
 
 
 Builder.load_string('''
+<KXModalBehaviorBackground>:
+    canvas:
+        Color:
+            rgba: self.overlay_color
+        Rectangle:
+            pos: 0, 0
+            size: self.size
 <KXModalBehavior>:
     pos_hint: {'center_x': .5, 'center_y': .5, }
-    canvas.before:
-        Color:
-            rgba: root.overlay_color
-        Rectangle:
-            pos: self.pos and self.to_local(0, 0)  # want to bind to 'self.pos'
-            size: (0, 0) if self.parent is None else self.parent.size
 ''')
+
+
+class KXModalBehaviorBackground(Factory.Widget):
+    overlay_color = ColorProperty()
+    def on_touch_down(self, touch):
+        return True
+    def on_touch_move(self, touch):
+        return True
+    def on_touch_up(self, touch):
+        return True
 
 
 class KXModalBehavior:
@@ -75,21 +82,29 @@ class KXModalBehavior:
 
         uid_touch = None
         uid_keyboard = None
+        background = KXModalBehaviorBackground(
+            overlay_color=self.overlay_color,
+            opacity=self.opacity,
+        )
+        self.bind(overlay_color=background.setter('overlay_color'))
+        self.bind(opacity=background.setter('opacity'))
         try:
             self.dispatch('on_pre_open')
             self._dismiss_event = dismiss_event = ak.Event()
             self.opacity = 0
-            uid_touch = self.fbind('on_touch_down', KXModalBehavior.__on_touch_down)
+            window.add_widget(background)
             window.add_widget(self)
             await ak.animate(self, opacity=1, d=.2)
+            uid_touch = self.fbind('on_touch_down', KXModalBehavior.__on_touch_down)
             uid_keyboard = window.fbind('on_keyboard', self._handle_keyboard)
             self.dispatch('on_open')
             return_value = await dismiss_event.wait()
             self.dispatch('on_pre_dismiss')
+            self.unbind_uid('on_touch_down', uid_touch)
             window.unbind_uid('on_keyboard', uid_keyboard)
             await ak.animate(self, opacity=0, d=.2)
             window.remove_widget(self)
-            self.unbind_uid('on_touch_down', uid_touch)
+            window.remove_widget(background)
             self.dispatch('on_dismiss')
             return return_value
         finally:
@@ -99,6 +114,8 @@ class KXModalBehavior:
                 window.unbind_uid('on_keyboard', uid_keyboard)
             if self.parent is not None:
                 window.remove_widget(self)
+            if background.parent is not None:
+                window.remove_widget(background)
 
     def leave(self, value):
         '''Leaves arbitrary value. The value will be the return-value of
@@ -120,21 +137,9 @@ class KXModalBehavior:
         self.leave(value=None)
 
     def __on_touch_down(self, touch):
-        ak.start(self._handle_touch(touch))
-        return True
-
-    async def _handle_touch(self, touch):
-        if not self.auto_dismiss or self.collide_point(*touch.opos):
-            super().on_touch_down(touch)
-            async for __ in ak.rest_of_touch_moves(
-                    self, touch, eats_touch_move=True, eats_touch_up=True, ):
-                super().on_touch_move(touch)
-            super().on_touch_up(touch)
-        else:
-            async for __ in ak.rest_of_touch_moves(
-                    self, touch, eats_touch_move=True, eats_touch_up=True, ):
-                pass
+        if self.auto_dismiss and not self.collide_point(*touch.opos):
             self.dismiss()
+            return True
 
     def on_pre_open(self):
         pass
