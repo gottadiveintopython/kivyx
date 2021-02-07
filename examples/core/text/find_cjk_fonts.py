e6454c086542040b5f50c68e21a829b745821ae8
gottadiveintopython <flow4re2c@gmail.com>
2021-02-07 16:04:19 +0900

    fix an example

--- examples/core/text/find_cjk_fonts.py
+++ examples/core/text/find_cjk_fonts.py
@@ -1,14 +1,13 @@
-from kivyx.utils import fontfinder
-from kivy.core.window import Window
+from kivyx.core.text.fontfinder import find_fonts_from_text
 
 sample_texts = {
     'zh-Hant': '經傳說',
     'zh-Hans': '经传说',
     'ko': '안녕',
-    'ja': 'あ伝説',
+    'ja': '経伝説',
 }
 
 for lang, text in sample_texts.items():
     print(f"\n---- list of fonts that support '{lang}' ----")
-    for font in fontfinder.find_fonts_from_text(text):
+    for font in find_fonts_from_text(text):
         print(font.name)
