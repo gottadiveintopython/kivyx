206f963db00ece652287dd2eb91bdb12d6490a84
Nattōsai Mitō <flow4re2c@gmail.com>
2023-08-30 07:11:48 +0900

    (api-break) remove suppress_event

--- src/kivyx/utils.py
+++ src/kivyx/utils.py
@@ -1,6 +1,5 @@
-__all__ = ('create_texture_from_text', 'suppress_event', )
+__all__ = ('create_texture_from_text', )
 
-from kivy.event import EventDispatcher
 from kivy.core.text import Label as CoreLabel
 from kivy.core.text.markup import MarkupLabel as CoreMarkupLabel
 
@@ -26,33 +25,3 @@ def create_texture_from_text(**label_kwargs):
     label = core(**label_kwargs)
     label.refresh()
     return label.texture
-
-
-class suppress_event:
-    '''
-    eventに結び付けられた関数を一時的に呼ばれないようにする context manager。
-    以下のコードでは ``on_press`` eventが発生してはいるものの ``suppress_event`` で囲っているため ``押されました`` とは出力されない。
-
-    .. code-block::
-
-       from kivy.uix.button import Button
-
-       btn = Button(on_press=lambda __: print("押されました"))
-       with suppress_event(btn, 'on_press'):
-           btn.dispatch('on_press')
-
-    .. note::
-
-       これをpropertyに対して用いる事はできない。
-    '''
-    __slots__ = ('_ed', '_e_name', '_bind_uid', )
-
-    def __init__(self, ed: EventDispatcher, e_name: str):
-        self._ed = ed
-        self._e_name = e_name
-
-    def __enter__(self, _f=lambda *__: True):
-        self._bind_uid = self._ed.fbind(self._e_name, _f)
-
-    def __exit__(self, *__):
-        self._ed.unbind_uid(self._e_name, self._bind_uid)
